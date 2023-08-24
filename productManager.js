910eeb7805c34c490a6f613f33d89009af450106
denise mayra <deniiise.mayra@gmail.com>
2023-08-24 20:48:58 -0300

    Refactor Code

--- productManager.js
+++ productManager.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 class ProductManager {
     constructor() {
-        this.path = './files/products.txt';
+        this.path = './files/products.json';
     }
 
     addProduct = async(object) => { 
@@ -16,7 +16,7 @@ class ProductManager {
                     }
                     const products = this.getProducts();
                     products.push(object);
-                    fs.promises.writeFile(this.path, JSON.stringify(products));
+                    fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
                     return {error: false, msg:`Archivo creado Correctamente con id: ${object.id}`};
                 } else {
                     return {error: true, msg:`Error: Codigo ${object.code} ya existente`};
@@ -77,83 +77,91 @@ class ProductManager {
     }
 
     updateProduct = async (id, objectModify) => {
-        const deleteResponse = await this.deleteProduct(id);
-        if (!deleteResponse.error){
-            objectModify = {...objectModify, id: id};
-            const response = await this.addProduct(objectModify);
-            if (!response.error){
-                return {error: false, msg:`Archivo modificado Correctamente`};
-            } else {
-                return {error: true, msg:'Hubo un error'};
+        try {
+            const deleteResponse = await this.deleteProduct(id);
+            if (!deleteResponse.error){
+                objectModify = {...objectModify, id: id};
+                const response = await this.addProduct(objectModify);
+                if (!response.error){
+                    return {error: false, msg:`Archivo modificado Correctamente`};
+                } else {
+                    return {error: true, msg:'Hubo un error'};
+                }
             }
+        } catch (error) {
+            throw error;
         }
     }
 
-    deleteProduct = (id) => {
+    deleteProduct = async (id) => {
         try{
             let products = this.getProducts();
             products = products.filter(prod => prod.id !== id);
 
-            fs.writeFileSync(this.path, JSON.stringify(products));
+            fs.writeFileSync(this.path, JSON.stringify(products, null, '\t'));
             return {error: false, msg:`Producto con id "${id}" eliminado correctamente`};
         } catch (error){
-            console.log('ERROR: ', error.message);
+            throw error;
         }
     }
 }
 
 async function test(){
-    const tecnology = new ProductManager();
-    const prod1 = {
-        title: 'Iphone 12',
-        desc: 'Iphone nuevo',
-        price: '980USD',
-        thumbnail: 'https://www.macstation.com.ar/img/productos/2493-2317-1.jpg',
-        stock: '10',
-        code: '0111'
-    };
-    const prod2Errrorcode = {
-        title: 'Iphone 6',
-        desc: 'Iphone nuevo',
-        price: '980USD',
-        thumbnail: 'https://www.macstation.com.ar/img/productos/2493-2317-1.jpg',
-        stock: '10',
-        code: '0111'
-    };
-    const prod2ErrrorMissing = {
-        title: 'Iphone 6',
-        desc: 'Iphone nuevo'
-    };
-    const prod2 = {
-        title: 'Iphone 11',
-        desc: 'Iphone nuevo',
-        price: '980USD',
-        thumbnail: 'https://www.macstation.com.ar/img/productos/2493-2317-1.jpg',
-        stock: '10',
-        code: '0222'
-    };
-    const prod1Modify = {
-        title: 'Iphone 12',
-        desc: 'modificado',
-        price: '1000USD',
-        thumbnail: 'https://www.macstation.com.ar/img/productos/2493-2317-1.jpg',
-        stock: '3',
-        code: '0000'
-    };
-    // no se puede enviar varios productos juntos porque el id va a ser el mismo ya que busca segun el largo del array,
-    // como se guarda con promesa no se llega a guardar el primero antes del segundo
-    // si es un id random si se puede
-
-    // console.log(tecnology.addProduct(prod1));
-    // console.log(tecnology.addProduct(prod2Errrorcode));
-    // console.log(tecnology.addProduct(prod2ErrrorMissing));
-    // console.log(tecnology.addProduct(prod2));
-    // console.log('----------- productos -----------');
-    // console.log(tecnology.getProducts());
-    // console.log('----------- producto con id 1 -----------');
-    // console.log(await tecnology.getProductById(1));
-    // console.log(await tecnology.updateProduct(2, prod1Modify ));
-    // console.log(await tecnology.getProductById(10));
+    try {
+        const tecnology = new ProductManager();
+        const prod1 = {
+            title: 'Iphone 12',
+            desc: 'Iphone nuevo',
+            price: '980USD',
+            thumbnail: 'https://www.macstation.com.ar/img/productos/2493-2317-1.jpg',
+            stock: '10',
+            code: '0111'
+        };
+        const prod2Errrorcode = {
+            title: 'Iphone 6',
+            desc: 'Iphone nuevo',
+            price: '980USD',
+            thumbnail: 'https://www.macstation.com.ar/img/productos/2493-2317-1.jpg',
+            stock: '10',
+            code: '0111'
+        };
+        const prod2ErrrorMissing = {
+            title: 'Iphone 6',
+            desc: 'Iphone nuevo'
+        };
+        const prod2 = {
+            title: 'Iphone 11',
+            desc: 'Iphone nuevo',
+            price: '980USD',
+            thumbnail: 'https://www.macstation.com.ar/img/productos/2493-2317-1.jpg',
+            stock: '10',
+            code: '0222'
+        };
+        const prod1Modify = {
+            title: 'Iphone 12',
+            desc: 'modificado',
+            price: '1000USD',
+            thumbnail: 'https://www.macstation.com.ar/img/productos/2493-2317-1.jpg',
+            stock: '3',
+            code: '0000'
+        };
+        // no se puede enviar varios productos juntos porque el id va a ser el mismo ya que busca segun el largo del array,
+        // como se guarda con promesa no se llega a guardar el primero antes del segundo
+        // si es un id random si se puede
+    
+        // console.log(tecnology.addProduct(prod1));
+        // console.log(tecnology.addProduct(prod2Errrorcode));
+        // console.log(tecnology.addProduct(prod2ErrrorMissing));
+        // console.log(tecnology.addProduct(prod2));
+        // console.log('----------- productos -----------');
+        // console.log(tecnology.getProducts());
+        // console.log('----------- producto con id 1 -----------');
+        // console.log(await tecnology.getProductById(1));
+        // console.log(await tecnology.updateProduct(2, prod1Modify ));
+        // console.log(await tecnology.getProductById(10));
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 test();
